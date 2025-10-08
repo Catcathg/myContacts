40ececb2265fbd79332d61cfd5a027c66b03cf13
Catcathg <catherine-guo@outlook.fr>
2025-10-09 00:29:16 +0200

    config

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -3,7 +3,7 @@ import "../index.css";
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import { API_URL } from "./config";
+import { API_URL } from "../config";
 
 const MailIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
